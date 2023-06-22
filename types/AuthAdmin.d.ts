b3d1b21a140f86f68d94f31395e941abe5f0011b
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-22 10:23:00 +0200

    🏷️ Add type for authAdmin

---
+++ types/AuthAdmin.d.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+
+export const SchemaAuthAdmin = z.object({
+    firstName: z.string().nonempty({ message: 'Prénom est obligatoire' }),
+    lastName: z.string().nonempty({ message: 'Nom est obligatoire' }),
+    role: z.string().nonempty({ message: 'Role est obligatoire' }),
+    email: z.string().email({ message: 'Email non valide' }),
+    password: z.string()
+        .nonempty({ message: 'Password est obligatoire' })
+        .min(8, { message: 'Mot de passe doit contenir au moins 8 caractères' })
+        .refine(
+        (value) => /[A-Z]/.test(value),
+        { message: 'Mot de passe doit contenir au moins une majuscule' }
+        )
+        .refine(
+        (value) => /\d/.test(value),
+        { message: 'Mot de passe doit contenir au moins un chiffre' }
+        )
+        .refine(
+        (value) => /[\W_]/.test(value),
+        { message: 'Mot de passe doit contenir au moins un caractère spécial' }
+        ),
+        confirmPassword: z.string().nonempty({ message: 'Confirm Password est obligatoire' })
+    }).refine((data) => data.password === data.confirmPassword, {
+        message: 'Les mots de passe doivent correspondre',
+        path: ['confirmPassword']
+    })
