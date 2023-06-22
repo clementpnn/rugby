5ea9dd5ff3645e028511bd27e2f730c8c64828e0
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-22 10:20:16 +0200

    🍱 Update Input

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -1,32 +1,31 @@
 'use client'
 
-import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form'
+import { InputHTMLAttributes } from 'react'
+import { FieldErrors, FieldError } from 'react-hook-form'
 
-interface InputProperties {
+interface InputProperties extends InputHTMLAttributes<HTMLInputElement> {
     label: string
     id: string
-    type?: string
-    required?: boolean
-    register: UseFormRegister<FieldValues>
     errors: FieldErrors
-    disabled?: boolean
 }
 
-const Input: React.FC<InputProperties> = ({label, id, type = 'text' , required, register, errors, disabled}) => {
+const Input: React.FC<InputProperties> = ({ label, id, type = 'text', errors, disabled, ...rest }) => {
+  const error = errors[id] as FieldError
   return (
-    <div>
-        <label htmlFor={id}>{label}</label>
-        <div className='mt-2'>
-            <input
-                id={id}
-                type={type}
-                autoComplete={id}
-                disabled={disabled}
-                {...register(id, { required })}
-                className={`block ring-1 ring-inset ring-gray-300 ${errors[id] && 'focus:ring-rose-500'} ${disabled && 'opacity-50 cursor-default'}`}
-            />
-        </div>
-    </div>
+    <>
+      <label htmlFor={id}>{label}</label>
+      <div className='mt-2'>
+          <input
+              id={id}
+              type={type}
+              autoComplete={id}
+              disabled={disabled}
+              {...rest}
+              className={`block ring-1 ring-inset ring-gray-300 ${errors[id] && 'focus:ring-rose-500'} ${disabled && 'opacity-50 cursor-default'}`}
+          />
+          {error && <p className="text-red-500 mt-2">{error.message}</p>}
+      </div>
+    </>
   )
 }
 
