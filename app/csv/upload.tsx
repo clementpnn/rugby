2a2313b66d8b1633502f1576e863cb724f594981
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-22 10:23:16 +0200

    ⚡️ Del axios

--- app/csv/upload.tsx
+++ app/csv/upload.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from 'react'
-import axios from 'axios'
 
 import useCSVToArray, { FileRow } from '@/hooks/useCSVToArray'
 import useCreatePassword from '@/hooks/useCreatePassword'
