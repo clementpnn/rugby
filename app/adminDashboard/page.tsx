644b4684fc649c865b4a2b1e37bf956d81eba3f8
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-22 22:37:46 +0200

    ✨ Add adminDashboard

--- app/adminDashboard/page.tsx
+++ app/adminDashboard/page.tsx
@@ -1,14 +1,13 @@
 // import getCurrentUser from '@/actions/getCurrentUser'
-
 import CSVForm from '@/components/forms/CSVForm'
-import { signOut } from 'next-auth/react'
+import SignOutButton from '@/components/buttons/SignOutButton'
 
 const page = async () => {
 //   const currentUser = await getCurrentUser()
 
   return (
     <>
-        <div className='cursor-pointer bg-blue-500' onClick={() => signOut()}>sign out</div>
+        <SignOutButton />
         <CSVForm />
     </>
   )
