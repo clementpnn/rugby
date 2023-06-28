04582cd0bf4cd89228f76db2f09fd52b89944cd4
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-28 14:13:10 +0200

    🎨 update ci
    Co-Authored-By: vitolinho <93949338+vitolinho@users.noreply.github.com>
    Co-Authored-By: AbdeDev <98222128+AbdeDev@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default async function Home() {
-  return ()
+  return (
+    <div>
+
+    </div>
+  )
 }
--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,31 +1,31 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
-      variants: {
+    variants: {
       variant: {
         accepted:
-          "border-transparent bg-green-100 p-1.5 text-green-600 rounded-2xl w-20 h-8 justify-center",
+          'border-transparent bg-green-100 p-1.5 text-green-600 rounded-2xl w-20 h-8 justify-center',
         rejected:
-          "border-transparent bg-red-100 p-1.5 text-red-600 rounded-2xl w-20 h-8 justify-center",
+          'border-transparent bg-red-100 p-1.5 text-red-600 rounded-2xl w-20 h-8 justify-center',
         progress:
-          "border-transparent bg-yellow-100 p-1.5 text-yellow-600 rounded-2xl w-20 h-8 justify-center",
-      },
-    },
+          'border-transparent bg-yellow-100 p-1.5 text-yellow-600 rounded-2xl w-20 h-8 justify-center'
+      }
+    }
   }
 )
 
-export interface BadgeProps
+export interface BadgeProperties
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({ className, variant, ...properties }: BadgeProperties) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <div className={cn(badgeVariants({ variant }), className)} {...properties} />
   )
 }
 
