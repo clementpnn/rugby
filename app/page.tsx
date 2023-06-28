ed879bba8f376d1aa3a24cf3b8d5629f36454213
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2023-06-28 16:48:52 +0200

    start for create profile card composant

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import ProfileCard from "@/components/CardComponents/DesktopProfileCard";
+
 export default async function Home() {
   return (
     <div>
-
+      <ProfileCard></ProfileCard>
     </div>
   )
 }
---
+++ components/CardComponents/DesktopProfileCard.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import * as React from "react"
+import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import {useState} from "react";
+
+
+const ProfileCard = () => {
+  return (
+    <div className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+     {/* <img  
+      className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+      src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"  alt="" />  */}
+
+    <div className="flex flex-col justify-start p-6">
+      <h5
+        className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+        Albus Trouduc
+      </h5>
+      <p className="text-xs text-neutral-500 dark:text-neutral-300">
+        Journaliste Poudlard
+      </p>
+      <button
+      type="button"
+      class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      data-te-ripple-init
+      data-te-ripple-color="light">
+      Button
+    </button>
+    </div>
+  </div>
+  );
+};
+
+export default ProfileCard;
