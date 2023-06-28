8340d4d4db55b72781da15a49a9160e76859f5ff
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-28 23:37:17 +0200

    🚚 Rename in camelCase

--- components/cards/profileCard.tsx
+++ components/cards/profileCard.tsx
@@ -7,7 +7,6 @@ import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 
-
 const ProfileCard = () => {
   return (
     <div className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
