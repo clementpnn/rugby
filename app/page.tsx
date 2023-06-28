16181e32ca55cbbc30e4ec28c0341daaf8a9fd71
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2023-06-28 14:01:32 +0200

    CardComposant Debut
    Co-Authored-By: Dassonville Morgane <99723167+Jun080@users.noreply.github.com>
    Co-Authored-By: vitolinho <93949338+vitolinho@users.noreply.github.com>
    Co-Authored-By: Clément <95907390+clementpnn@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import getCurrentUser from '@/actions/getCurrentUser'
 import SignInModal from '@/components/modals/SignInModal'
+import { Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 // import Modal from '@/components/modals/Modal'
 // import useModal from '@/hooks/useModal'
 // import { useState } from 'react'
@@ -24,6 +27,40 @@ export default async function Home() {
     <>
       <SignInModal currentUser={currentUser} />
       {/* <Modal  disabled={false} isOpen={modal.isOpen} title='Login' actionLabel='Continue' onClose={modal.onClose} onSubmit={handleSubmit(onSubmit)} /> */}
+
+      <Card className='w-[350px]'>
+        <div className="bg-blue6 p-4 flex items-center justify-between">
+          <div className="flex items-center">
+            <h3 className="text-neutral0 text-lg font-semibold">Tournament Pass</h3>
+          </div>
+          <div className="flex items-center">
+            <img src="logo.png" alt="Logo" className="h-6 w-6 ml-2" />
+          </div>
+        </div>
+        <CardHeader className='flex items-center'>
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                <AvatarFallback>Profile picture</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="ml-2">
+              <div className="flex flex-col">
+                <CardTitle>Abde</CardTitle>
+                <CardDescription>Journaliste Poudlard</CardDescription>
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+        </CardContent>
+        <CardFooter className='flex justify-between'>
+          <Button variant='s_outline' size={'sm'}>[ => Logoutx</Button>
+        </CardFooter>
+      </Card>
+
+
     </>
   )
 }
