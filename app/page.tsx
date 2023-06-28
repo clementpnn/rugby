aabf497989858dc19130e8d988d4f82bb5ab6d10
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-28 10:51:20 +0200

    ✨ Create modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import getCurrentUser from '@/actions/getCurrentUser'
 import SignInModal from '@/components/modals/SignInModal'
+import Modal from '@/components/modals/Modal'
 import { Button } from '@/components/ui/button'
+import useModal from '@/hooks/useModal'
+// import { useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
 export default async function Home() {
   const currentUser = await getCurrentUser()
+  const modal = useModal()
+  // const [isLoading, setIsLoading] = useState(false)
+
+  const { handleSubmit } = useForm<FieldValues>({
+    defaultValues: {
+      email: ''
+    }
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    // setIsLoading(false)
+  }
+
   return (
     <>
       <SignInModal currentUser={currentUser} />
+      <Modal  disabled={false} isOpen={modal.isOpen} title='Login' actionLabel='Continue' onClose={modal.onClose} onSubmit={handleSubmit(onSubmit)} />
     </>
   )
 }
