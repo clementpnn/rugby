351a97610e6161cbf03a093a1fc921f2c8fc9a13
Clément <95907390+clementpnn@users.noreply.github.com>
2023-06-28 22:56:53 +0200

    ♻️ Add options props

--- components/inputs/Select.tsx
+++ components/inputs/Select.tsx
@@ -2,19 +2,28 @@
 
 import { ChangeEvent, SelectHTMLAttributes } from 'react'
 
+interface SelectOption {
+  value: string
+  label: string
+}
+
 interface SelectProperties extends SelectHTMLAttributes<HTMLSelectElement> {
   label: string
   name: string
   value: string
+  options: SelectOption[]
   onChange: (event: ChangeEvent<HTMLSelectElement>) => void
 }
 
-const Select: React.FC<SelectProperties> = ({ label, name, value, onChange, disabled }) => (
+const Select: React.FC<SelectProperties> = ({ label, name, value, onChange, options, disabled }) => (
   <div className='block'>
     <label>{label}</label>
     <select name={name} disabled={disabled} onChange={onChange} value={value}>
-      <option value='ADMIN'>Admin</option>
-      <option value='DEV'>Dev</option>
+    {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.label}
+        </option>
+    ))}
     </select>
   </div>
 )
