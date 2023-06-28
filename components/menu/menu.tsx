9a0a5e586b1faa9f50c4c12d38f00cecb95aa71e
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2023-06-28 15:43:49 +0200

    update color

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -20,7 +20,7 @@ function Menu() {
 
     return (
         <div className="flex justify-center">
-            <div className="fixed bottom-8 bg-blue-600 w-[calc(100vw-40px)] h-fit pl-5 pr-1.5 py-2 rounded-lg max-w-[350px]">
+            <div className="fixed bottom-8 bg-blue6 w-[calc(100vw-40px)] h-fit pl-5 pr-1.5 py-2 rounded-lg max-w-[350px]">
                 {showDrawer &&
                     <div className="grid grid-cols-1  gap-y-2 mt-5 mb-4 pr-3.5" onClick={handleShowDrawer}>
                         <button className="text-left px-6 py-3 rounded-md border border-blue-400 text-white hover:bg-blue-500 focus:bg-blue-400">Pools</button>
