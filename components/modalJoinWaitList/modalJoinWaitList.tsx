09e008f27e200e3b22acf9e69cf9b4e201043ea6
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-06-29 16:06:16 +0200

    i need to pull

---
+++ components/modalJoinWaitList/modalJoinWaitList.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { Button } from "../ui/button"
+
+const ModalJoinWaitList = () => {
+    return (
+        <div className="h-screen w-screen flex justify-center items-center box-border">
+            <div className='w-[535px] h-[416px] bg-blue1 rounded-[12px]'>
+                <div className="h-[128px] flex flex-row justify-between py-[40px] px-[32px] border-b-4 border-blue3">
+                    {/* mettre bonne font ?? */}
+                    <span className="text-blue6">QUARTER FINAL</span>
+                    {/* button icon ?? */}
+                    <Button variant={'outline'} size={'sIcon'}>X</Button>
+                </div>
+                <div className='h-fit flex flex-col ml-[32px] mt-[32px] mr-[32px] mb-[40px] gap-[32px]'>
+                    <div className='w-[471px] h-[132px] flex flex-col gap-[24px]'>
+                        <div className="h-[64px] flex flex-row items-end justify-between">
+                            <div className="text-blue6 flex flex-col justify-start items-start gap-[4px]">
+                                <span>20:45</span>
+                                <span>Saturday October 10, 2023</span>
+                            </div>
+                            <div className="text-blue6 flex justify-end">
+                                <span>Stade de Marseille</span>
+                            </div>
+                        </div>
+                        <div className="h-[44px] flex flex-row gap-[16px] items-center">
+                            <div className="bg-blue2 w-[203px] flex flex-row gap-[12px] px-[16px] py-[8px] items-center rounded-lg">
+                                <div>FLAG</div>
+                                <span>New Zealand</span>
+                            </div>
+                            <span className="text-blue6">VS</span>
+                            <div className="bg-blue2 w-[203px] flex flex-row gap-[12px] px-[16px] py-[8px] items-center rounded-lg">
+                                <div>FLAG</div>
+                                <span>New Zealand</span>
+                            </div>
+                        </div>
+                    </div>
+                    <Button className='m-0' variant={'fill'} size={'lg'}>Join Wait List</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ModalJoinWaitList
