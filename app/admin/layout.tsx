3beb4890f2bf7aa560800937f6581bd203405c95
Clément <95907390+clementpnn@users.noreply.github.com>
2023-07-03 17:13:38 +0200

    ♻️ Update admin create route

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,4 @@
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function AdminCreateLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       {children}
