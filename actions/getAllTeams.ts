4717dbbf2984d5b010c04f2b74db052b64fa3a41
Clément <95907390+clementpnn@users.noreply.github.com>
2023-07-05 10:11:54 +0200

    🔥 remove

--- actions/getAllTeams.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '@/libs/prismadb'
-
-export default async function getAllTeams() {
-  try {
-    return await prisma.team.findMany()
-  } catch (error: any) {
-    throw new Error(error)
-  }
-}
