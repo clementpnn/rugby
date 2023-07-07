dbc225884743409e8cc2db9a1927300aa716f6e6
Clément <95907390+clementpnn@users.noreply.github.com>
2023-07-07 08:42:29 +0200

    🚚 Change section to tribune

--- app/admin/create/section/layout.tsx
+++ app/admin/create/section/layout.tsx
@@ -3,7 +3,7 @@ export const metadata = {
   description: 'End of school year project'
 }
 
-export default function AdminCreateSectionLayout({ children }: { children: React.ReactNode }) {
+export default function AdminCreateTribuneLayout({ children }: { children: React.ReactNode }) {
     return (
       <div>
         {children}
--- app/admin/create/section/page.tsx
+++ app/admin/create/section/page.tsx
@@ -1,11 +1,11 @@
 import getMatch from '@/actions/getMatchs'
-import SectionForm from '@/components/forms/sectionForm'
+import TribuneForm from '@/components/forms/tribuneForm'
 
 const page = async () => {
   const matchs = await getMatch()
   return (
     <> 
-      <SectionForm matchs={matchs} />
+      <TribuneForm matchs={matchs} />
     </>
   )
 }
