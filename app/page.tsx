bb83cda6c91189b03224550d45ca36708d05ecf3
Victor Huang <huang.victor.mail@gmail.com>
2023-07-08 19:22:56 +0200

    💄 UI de button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import LogoutModal from "@/components/modals/logoutModal";
+import Container from '@/components/containers/container'
+import {Button} from '@/components/ui/button'
+// import { useState } from 'react';
+
 
 export default async function Home() {
+  // eslint-disable-next-line no-unused-vars
+  // const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   return (
-    <div>
-        <LogoutModal />
-    </div>
+    // <div className='bg-neutral0 h-screen'>
+    //    <Button variant='link' size='sm'>Salut</Button>
+    //    {/* <Button variant='link' size='sm' disabled={isButtonDisabled}>Salut</Button> */}
+
+    // </div>
+    <Container>
+      <Button variant='disabled' size='sm'>Salut</Button>
+    </Container>
   )
 }
--- components/containers/container.tsx
+++ components/containers/container.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 interface ContainerProperties {
     children: React.ReactNode
 }
