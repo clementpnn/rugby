a4d1489368b5112cd844afbd4243d88d9db53357
Clément <95907390+clementpnn@users.noreply.github.com>
2023-07-08 13:44:01 +0200

    ✨ Create container

---
+++ components/containers/container.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface ContainerProperties {
+    children: React.ReactNode
+}
+
+const Container: React.FC<ContainerProperties> = ({ children }) => {
+    return <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{children}</div>
+}
+
+export default Container
