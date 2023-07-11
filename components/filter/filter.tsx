811e9568d715a06961262fc4a923070100007092
Clément <95907390+clementpnn@users.noreply.github.com>
2023-07-11 22:23:13 +0200

    🎨 Fix lint

--- components/filter/filter.tsx
+++ components/filter/filter.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react'
 import { useState } from 'react'
 
-
 type PoolData = {
   pool: string;
   teams: string[];
@@ -12,28 +11,28 @@ type PoolData = {
 const TeamImage: React.FC = () => {
   return (
     <div className='w-8 h-8 rounded-full bg-blue-500'></div>
-  );
-};
+  )
+}
 
 const Filter: React.FC = () => {
-  const [showPool, setShowPool] = useState(false);
-  const [activeTab, setActiveTab] = useState('');
+  const [ showPool, setShowPool ] = useState( false )
+  const [ activeTab, setActiveTab ] = useState( '' )
 
   const handlePoolClick = () => {
-    setShowPool(!showPool);
-    setActiveTab('pool');
-  };
+    setShowPool( !showPool )
+    setActiveTab( 'pool' )
+  }
 
-  const handleTabClick = (tab: string) => {
-    setActiveTab(tab);
-  };
+  const handleTabClick = ( tab: string ) => {
+    setActiveTab( tab )
+  }
 
   const poolData: PoolData[] = [
-    { pool: 'A', teams: ['TeamA1', 'TeamA2', 'TeamA3', 'TeamA4', 'TeamA5'] },
-    { pool: 'B', teams: ['TeamB1', 'TeamB2', 'TeamB3', 'TeamB4', 'TeamB5'] },
-    { pool: 'C', teams: ['TeamC1', 'TeamC2', 'TeamC3', 'TeamC4', 'TeamC5'] },
-    { pool: 'D', teams: ['TeamD1', 'TeamD2', 'TeamD3', 'TeamD4', 'TeamD5'] }
-  ];
+    { pool: 'A', teams: [ 'TeamA1', 'TeamA2', 'TeamA3', 'TeamA4', 'TeamA5' ] },
+    { pool: 'B', teams: [ 'TeamB1', 'TeamB2', 'TeamB3', 'TeamB4', 'TeamB5' ] },
+    { pool: 'C', teams: [ 'TeamC1', 'TeamC2', 'TeamC3', 'TeamC4', 'TeamC5' ] },
+    { pool: 'D', teams: [ 'TeamD1', 'TeamD2', 'TeamD3', 'TeamD4', 'TeamD5' ] }
+  ]
 
   return (
     <div className='flex items-start justify-start h-882 w-423'>
@@ -52,7 +51,7 @@ const Filter: React.FC = () => {
                   className={`cursor-pointer ${
                     activeTab === 'pool' ? 'text-white' : ''
                   }`}
-                  onClick={() => handleTabClick('pool')}
+                  onClick={() => handleTabClick( 'pool' )}
                 >
                   Pool
                 </span>
@@ -61,7 +60,7 @@ const Filter: React.FC = () => {
                 className={`px-6 py-3 border bg-transparent text-blue-500 rounded-lg ${
                   activeTab === 'knockout' ? 'bg-blue-500 text-white' : ''
                 }`}
-                onClick={() => setActiveTab('knockout')}
+                onClick={() => setActiveTab( 'knockout' )}
               >
                 Knock-out
               </button>
@@ -71,13 +70,13 @@ const Filter: React.FC = () => {
             <h2 className='text-2xl text-blue-500 font-bold mb-4'>Filter</h2>
             {showPool && (
               <div className='flex flex-col gap-2'>
-                {poolData.map((poolItem) => (
+                {poolData.map( ( poolItem ) => (
                   <div
                     key={poolItem.pool}
                     className={`border-b border-gray-300 ${
                       activeTab === `pool${poolItem.pool}` ? 'cursor-pointer' : ''
                     }`}
-                    onClick={() => handleTabClick(`pool${poolItem.pool}`)}
+                    onClick={() => handleTabClick( `pool${poolItem.pool}` )}
                   >
                     <div className='flex items-center'>
                       <h1
@@ -91,23 +90,23 @@ const Filter: React.FC = () => {
                     </div>
                     {activeTab === `pool${poolItem.pool}` && (
                       <ul className='mt-4 space-y-3'>
-                        {poolItem.teams.map((team, index) => (
+                        {poolItem.teams.map( ( team, index ) => (
                           <li key={index} className='flex items-center gap-2'>
                             <TeamImage /> {team}
                           </li>
-                        ))}
+                        ) )}
                       </ul>
                     )}
                   </div>
-                ))}
+                ) )}
               </div>
             )}
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const App: React.FC = () => {
   return (
@@ -117,7 +116,7 @@ const App: React.FC = () => {
         <Filter />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
