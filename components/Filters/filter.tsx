5b7ae05b6cbf1ee67d92bbc588f375e83d087b6a
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2023-07-11 15:46:12 +0200

    💄update Filter component and add Pool UI

--- components/Filters/filter.tsx
+++ components/Filters/filter.tsx
@@ -6,151 +6,120 @@ import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 
 
-type Match = {
-  team1: string;
-  team2: string;
-};
-
-type PoolCategory = {
-  category: string;
+type PoolData = {
+  pool: string;
   teams: string[];
 };
 
-const FilterComponent: React.FC = () => {
-  const [phase, setPhase] = useState<'Pool' | 'Knockout'>('Pool');
-  const [selectedPool, setSelectedPool] = useState<string>('');
-  const [selectedTeam, setSelectedTeam] = useState<string>('');
-  const [matches, setMatches] = useState<Match[]>([]);
+const TeamImage: React.FC = () => {
+  return (
+    <div className='w-8 h-8 rounded-full bg-blue-500'></div>
+  );
+};
 
-  const poolCategories: PoolCategory[] = [
-    { category: 'Pool A', teams: ['Team1A', 'Team2A', 'Team3A', 'Team4A', 'Team5A'] },
-    { category: 'Pool B', teams: ['Team1B', 'Team2B', 'Team3B', 'Team4B', 'Team5B'] },
-    { category: 'Pool C', teams: ['Team1C', 'Team2C', 'Team3C', 'Team4C', 'Team5C'] },
-    { category: 'Pool D', teams: ['Team1D', 'Team2D', 'Team3D', 'Team4D', 'Team5D'] },
+const Filter: React.FC = () => {
+  const [showPool, setShowPool] = useState(false);
+  const [activeTab, setActiveTab] = useState('');
+
+  const handlePoolClick = () => {
+    setShowPool(!showPool);
+    setActiveTab('pool');
+  };
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+  const poolData: PoolData[] = [
+    { pool: 'A', teams: ['TeamA1', 'TeamA2', 'TeamA3', 'TeamA4', 'TeamA5'] },
+    { pool: 'B', teams: ['TeamB1', 'TeamB2', 'TeamB3', 'TeamB4', 'TeamB5'] },
+    { pool: 'C', teams: ['TeamC1', 'TeamC2', 'TeamC3', 'TeamC4', 'TeamC5'] },
+    { pool: 'D', teams: ['TeamD1', 'TeamD2', 'TeamD3', 'TeamD4', 'TeamD5'] }
   ];
 
-  const handlePoolButtonClick = () => {
-    setPhase('Pool');
-    setSelectedPool('');
-    setSelectedTeam('');
-    setMatches([]);
-  };
-
-  const handleKnockoutButtonClick = () => {
-    setPhase('Knockout');
-    setSelectedPool('');
-    setSelectedTeam('');
-    setMatches([]);
-  };
-
-  const handlePoolCategoryClick = (category: string) => {
-    setSelectedPool(category);
-    setSelectedTeam('');
-    setMatches([]);
-  };
-
-  const handleTeamClick = (team: string) => {
-    setSelectedTeam(team);
-    const selectedPoolTeams = poolCategories.find((category) => category.category === selectedPool)?.teams || [];
-    const otherTeams = selectedPoolTeams.filter((t) => t !== team);
-    const poolMatches: Match[] = otherTeams.map((t) => ({ team1: team, team2: t }));
-    setMatches(poolMatches);
-  };
-
-  const handleQuarterFinalButtonClick = () => {
-    setSelectedPool('');
-    setSelectedTeam('');
-    setMatches([
-      { team1: 'Quarter1 Team1', team2: 'Quarter1 Team2' },
-      { team1: 'Quarter2 Team1', team2: 'Quarter2 Team2' },
-      { team1: 'Quarter3 Team1', team2: 'Quarter3 Team2' },
-      { team1: 'Quarter4 Team1', team2: 'Quarter4 Team2' },
-    ]);
-  };
-
-  const handleSemiFinalButtonClick = () => {
-    setSelectedPool('');
-    setSelectedTeam('');
-    setMatches([
-      { team1: 'Semi1 Team1', team2: 'Semi1 Team2' },
-      { team1: 'Semi2 Team1', team2: 'Semi2 Team2' },
-    ]);
-  };
-
-  const handleFinalButtonClick = () => {
-    setSelectedPool('');
-    setSelectedTeam('');
-    setMatches([{ team1: 'Final Team1', team2: 'Final Team2' }]);
-  };
-
   return (
-    <div>
-      <h1 style={{ fontWeight: 'bold' }}>Title</h1>
-
-      <button onClick={handlePoolButtonClick}>Pool</button>
-      <button onClick={handleKnockoutButtonClick}>Knockout</button>
-
-      {phase === 'Pool' && (
-        <div>
-          {poolCategories.map((category) => (
-            <button
-              key={category.category}
-              onClick={() => handlePoolCategoryClick(category.category)}
-              className={selectedPool === category.category ? 'active' : ''}
-            >
-              {category.category}
-            </button>
-          ))}
-        </div>
-      )}
-
-      {phase === 'Knockout' && (
-        <div>
-          <button onClick={handleQuarterFinalButtonClick}>Quarter Final</button>
-          <button onClick={handleSemiFinalButtonClick}>Semi Final</button>
-          <button onClick={handleFinalButtonClick}>Final</button>
-        </div>
-      )}
-
-      {phase === 'Pool' && selectedPool !== '' && selectedTeam === '' && (
-        <div>
-          {poolCategories
-            .find((category) => category.category === selectedPool)
-            ?.teams.map((team) => (
-              <button
-                key={team}
-                onClick={() => handleTeamClick(team)}
-                className={selectedTeam === team ? 'active' : ''}
+    <div className='flex items-start justify-start h-882 w-423'>
+      <div className='w-423 h-440 bg-gray-100 p-4'>
+        <div className='flex flex-col gap-4'>
+          <div className='w-423 h-240 bg-gray-100 p-4 ml-4 border-b-0'>
+            <h1 className='text-3xl text-blue-500 font-bold mb-4'>Planning</h1>
+            <div className='flex gap-4'>
+              <Button
+                className={`px-6 py-3 border bg-transparent text-blue-500 rounded-lg ${
+                  activeTab === 'pool' ? 'bg-blue-500 text-white cursor-pointer' : ''
+                }`}
+                onClick={handlePoolClick}
               >
-                {team}
-              </button>
-            ))}
-        </div>
-      )}
-
-      {selectedTeam !== '' && (
-        <div>
-          <h2>{selectedTeam} Matches:</h2>
-          {matches.map((match, index) => (
-            <div key={index}>
-              <span>{match.team1}</span> vs <span>{match.team2}</span>
+                <span
+                  className={`cursor-pointer ${
+                    activeTab === 'pool' ? 'text-white' : ''
+                  }`}
+                  onClick={() => handleTabClick('pool')}
+                >
+                  Pool
+                </span>
+              </Button>
+              <Button
+                className={`px-6 py-3 border bg-transparent text-blue-500 rounded-lg ${
+                  activeTab === 'knockout' ? 'bg-blue-500 text-white' : ''
+                }`}
+                onClick={() => setActiveTab('knockout')}
+              >
+                Knock-out
+              </Button>
             </div>
-          ))}
+          </div>
+          <div className='w-423 h-240 bg-gray-100 p-4 ml-4'>
+            <h2 className='text-2xl text-blue-500 font-bold mb-4'>Filter</h2>
+            {showPool && (
+              <div className='flex flex-col gap-2'>
+                {poolData.map((poolItem) => (
+                  <div
+                    key={poolItem.pool}
+                    className={`border-b border-gray-300 ${
+                      activeTab === `pool${poolItem.pool}` ? 'cursor-pointer' : ''
+                    }`}
+                    onClick={() => handleTabClick(`pool${poolItem.pool}`)}
+                  >
+                    <div className='flex items-center'>
+                      <h1
+                        className={`text-3xl text-blue-500 font-bold ${
+                          activeTab === `pool${poolItem.pool}` ? 'text-blue-500 cursor-pointer' : ''
+                        }`}
+                      >
+                        {poolItem.pool}
+                      </h1>
+                      <p className='ml-1'>Pool</p>
+                    </div>
+                    {activeTab === `pool${poolItem.pool}` && (
+                      <ul className='mt-4 space-y-3'>
+                        {poolItem.teams.map((team, index) => (
+                          <li key={index} className='flex items-center gap-2'>
+                            <TeamImage /> {team}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-      )}
-
-      {phase === 'Knockout' && matches.length > 0 && (
-        <div>
-          <h2>Matches:</h2>
-          {matches.map((match, index) => (
-            <div key={index}>
-              <span>{match.team1}</span> vs <span>{match.team2}</span>
-            </div>
-          ))}
-        </div>
-      )}
+      </div>
     </div>
   );
 };
 
-export default FilterComponent;
+const App: React.FC = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <div className='w-423 h-240 bg-gray-100 p-48'>
+        <h5 className='text-3xl font-bold mb-4'>Filter</h5>
+        <Filter />
+      </div>
+    </div>
+  );
+};
+
+export default App;
