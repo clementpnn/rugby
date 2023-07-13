b8d16ef70c716194439a926b987c39cf88bccf35
Clément <95907390+clementpnn@users.noreply.github.com>
2023-07-13 15:28:41 +0200

    🚩 Add test (with bug)

--- components/buttons/button.tsx
+++ components/buttons/button.tsx
@@ -10,27 +10,6 @@ interface ButtonProperties extends React.ButtonHTMLAttributes<HTMLButtonElement>
   children?: React.ReactNode
 }
 
-// const Button: React.FC<ButtonProperties> = ({ type= 'button', fullWidth, children, onclick, secondary, danger, disabled }) => {
-//   return (
-//     <button
-//       onClick={onclick}
-//       type={type}
-//       disabled={disabled}
-//       className={`
-//         ${disabled && 'opacity-50 cursor-default'}
-//         ${fullWidth && 'w-full'}
-//         ${secondary ? 'text-gray-900' : 'text-white'}
-//         ${danger && 'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600'}
-//         ${!secondary && !danger && 'bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600'}
-//         `}
-//     >
-//       {children}
-//     </button>
-//   )
-// }
-
-// export default Button
-
 const Button: React.FC<ButtonProperties> = ( {
   variant,
   size,
