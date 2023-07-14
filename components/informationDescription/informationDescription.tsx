ce65df34357cb51eba0e2b5e76752e2ad6019792
Victor Huang <huang.victor.mail@gmail.com>
2023-07-14 23:34:19 +0200

    🩹 Fix Font

--- components/informationDescription/informationDescription.tsx
+++ components/informationDescription/informationDescription.tsx
@@ -13,7 +13,7 @@ const InformationDescription: React.FC<InformationDescriptionProperties> = ( { d
     <div className='flex flex-col gap-y-3'>
       <div className='flex flex-row justify-between'>
         <p className='text-blue6 h4-barlow-m'>{data.shortcut}</p>
-        <p className='text-blue4 h6-lato-d'>{data.title}</p>
+        <p className='text-blue4 h6-inter-d'>{data.title}</p>
       </div>
       <p className='text-blue9 base-md'>{data.description}</p>
     </div>
