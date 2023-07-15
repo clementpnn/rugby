3bd25b7860088973ada982ce57d49247c821fef2
Victor Huang <huang.victor.mail@gmail.com>
2023-07-16 00:50:24 +0200

    🚸 Database input fix

--- components/table/dataTable.tsx
+++ components/table/dataTable.tsx
@@ -87,9 +87,9 @@ export function DataTable<TData, TValue>( {
       <div className="flex items-center py-4">
         <InputUI
           placeholder="Filter emails..."
-          value={( table.getColumn( 'email' )?.getFilterValue() as string ) ?? ''}
+          value={( table.getColumn( 'name' )?.getFilterValue() as string ) ?? ''}
           onChange={( event ) =>
-            table.getColumn( 'email' )?.setFilterValue( event.target.value )
+            table.getColumn( 'name' )?.setFilterValue( event.target.value )
           }
           className="max-w-sm"
         />
