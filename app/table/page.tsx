4b63f2936e651e14db29b3658109aa473398c42e
Victor Huang <huang.victor.mail@gmail.com>
2023-07-17 14:04:27 +0200

    🚧 Modal

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,16 +1,17 @@
-import { DataTable } from '@/components/table/dataTable'
-import { columns } from './columns'
-import { data } from './data'
+// import { DataTable } from '@/components/table/dataTable'
+// import { columns } from './columns'
+// import { data } from './data'
 import { DatePicker } from '@/components/datePicker/datePicker'
 // import { DataTableDemo } from '@/components/table/testTable'
+import ModalJoinWaitList from '@/components/modals/modalJoinWaitList'
 
 const page = async () => {
-
   return (
     <>
-      <DataTable columns={columns} data={data}/>
+      {/* <DataTable columns={columns} data={data}/> */}
       {/* <DataTableDemo /> */}
       <DatePicker></DatePicker>
+      <ModalJoinWaitList></ModalJoinWaitList>
     </>
   )
 }
