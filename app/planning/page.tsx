3c0133ced4f462040e2f33cf93d632fbbeeed0b3
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2023-07-18 16:14:07 +0200

    💄add planning page ui

--- app/planning/page.tsx
+++ app/planning/page.tsx
@@ -8,8 +8,26 @@ interface IParameters {
 
 const page = async ( { params }: {params: IParameters} ) => {
   return (
-    <><><Navbar />
-      <Empty /></><Filter /></>
+<div className="flex flex-col h-screen">
+  <div className="sticky top-0">
+    <Navbar />
+    <div className="border-t-2"></div>
+  </div>
+
+  <div className="flex flex-col lg:flex-row flex-grow">
+    <div className="flex-1 border-r-2 lg:border-r-1">
+      <Empty />
+    </div>
+
+    <div className="lg:w-1/3 self-start lg:self-end">
+      <div className="lg:mt-10">
+        <Filter />
+      </div>
+    </div>
+  </div>
+</div>
+
+
   )
 }
 
