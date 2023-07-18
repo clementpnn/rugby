30404a03c9ae9a0c873d5acf9d763ec1cd234670
Clément <95907390+clementpnn@users.noreply.github.com>
2023-07-19 00:04:21 +0200

    ✨ Add reset password

---
+++ app/resetPassword/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Rugby User',
+  description: 'End of school year project'
+}
+
+export default function resetPasswordLayout( { children }: { children: React.ReactNode } ) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
---
+++ app/resetPassword/page.tsx
@@ -0,0 +1,10 @@
+import EmailForm from '@/components/forms/emailForm'
+
+const page = async () => {
+
+  return (
+    <EmailForm />
+  )
+}
+
+export default page
