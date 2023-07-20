c368a2a697b1405e53053bd8ae985d239edce3f8
Victor Huang <huang.victor.mail@gmail.com>
2023-07-21 00:12:25 +0200

    Creation ModalCreateMatch

---
+++ components/modals/ModalCreateMatch.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Modal from './dialog'
+import Button from '../buttons/button'
+
+const ModalCreateMatch = () => {
+  return (
+    <>
+      <Modal title='Creat Match' action={<Button size='lg'>Create Match</Button>}>
+        Salut
+      </Modal>
+    </>
+  )
+}
+
+export default ModalCreateMatch
