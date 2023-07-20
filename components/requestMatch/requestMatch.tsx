3b7be640e3849631574fd93d177e49b03cd59051
Victor Huang <huang.victor.mail@gmail.com>
2023-07-20 04:01:17 +0200

    💄 Fix request match ui

--- components/requestMatch/requestMatch.tsx
+++ components/requestMatch/requestMatch.tsx
@@ -21,17 +21,19 @@ const RequestMatch: React.FC<MatchProperties> = ( { data, stateClass, state } ) 
   const imgCountryRight = getByValue( data.matchTeams[1].team )
   return (
     <div className="flex justify-between flex-col md:flex-row items-start md:items-center border-y-[1px] w-full h-fit px-5 md:px-20 py-6">
-      <div className="w-[180px] flex flex-col items-start gap-y-2">
+      <div className="w-[280px] flex flex-col items-start gap-y-2">
         <div className="md:h4-barlow-m h5-barlow-m text-blue6">{ data.time }</div>
-        <div className="md:label-md label-sm text-blue6">{ data.stadiumName }</div>
+        <div className="label-sm text-blue6">{ data.stadiumName }</div>
       </div>
       <div className="flex items-center justify-center gap-x-10 md:w-hug w-full md:mt-0">
         <div className="flex items-center justify-end">
           <Image
             src= {imgCountryLeft?.flag || '/placeholder-image.png'}
             width={56}
-            height={150}
+            height={56}
             alt="flag"
+            className='rounded-full'
+            style={{ boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.1)' }}
           />
           <div className="text-blue9 h5-barlow-m uppercase text-left w-14 md:ml-4">
             { data.matchTeams[0].team.slice( 0, 3 ) }
@@ -54,12 +56,14 @@ const RequestMatch: React.FC<MatchProperties> = ( { data, stateClass, state } ) 
           <Image
             src= {imgCountryRight?.flag || '/placeholder-image.png'}
             width={56}
-            height={150}
+            height={56}
             alt="flag"
+            className='rounded-full'
+            style={{ boxShadow: '0px 0px 14px rgba(0, 0, 0, 0.1)' }}
           />
         </div>
       </div>
-      <div className="flex flex-row w-[180px] h-9 justify-end items-center md:mr-0 mr-4 md:mt-0 mt-3 right-0 absolute md:relative">
+      <div className="flex flex-row w-[280px] h-9 justify-end items-center md:mr-0 mr-4 md:mt-0 mt-3 right-0 absolute md:relative">
         <Badge size='md' variant={stateClass}>{ state }</Badge>
       </div>
     </div>
