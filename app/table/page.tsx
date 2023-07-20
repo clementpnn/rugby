54eeae7a8f8e346ec89fd51be2cb55f7d95b6b4f
Jun080 <m_dassonville3@hetic.eu>
2023-07-20 11:06:26 +0200

    add import

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,6 +1,6 @@
-
 import AdminContainer from '@/components/containers/adminContainer'
 import Import from '@/components/import/import'
+import TableImport from '@/components/import/tableImpot'
 
 const page = async () => {
   // const data = await getMatchById(1)
@@ -10,7 +10,7 @@ const page = async () => {
       <div>
         <AdminContainer>
           <Import/>
-          {/*<DataTable columns={columns} data={data}/>*/}
+          <TableImport/>
         </AdminContainer>
       </div>
     </>
