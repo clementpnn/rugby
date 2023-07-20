7f0ac18e916662d7690bf795a5f59c8b4e8b39a2
Salma WADOUACHI <s_wadouachi@hetic.eu>
2023-07-20 15:49:17 +0200

    fix lint /knockoutslayout/page

--- app/knockouts/knockoutslayout.tsx
+++ app/knockouts/knockoutslayout.tsx
@@ -6,90 +6,90 @@ export interface Teams{
   data: Team[]
 }
 export const TeamsData: Teams[] = [
-{
-  data: [
-    {
-      teamName: 'FRANCE',
-      result: 'W'
-    },
-    {
-      teamName: 'NEW_ZEALAND',
-      result: 'L'
-    }
-  ]
-},
-{
-  data: [
-    {
-      teamName: 'ITALY',
-      result: 'L'
-    },
-    {
-      teamName: 'URUGUAY',
-      result: 'W'
-    }
-  ]
-},
-{
-  data: [
-    {
-      teamName: 'SOUTH_AFRICA',
-      result: 'L'
-    },
-    {
-      teamName: 'AUSTRALIA',
-      result: 'W'
-    }
-  ]
-},
-{
-  data: [
-    {
-      teamName: 'PORTUGAL',
-      result: 'W'
-    },
-    {
-      teamName: 'ARGENTINA',
-      result: 'L'
-    }
-  ]
-},
-{
-  data: [
-    {
-      teamName: 'FRANCE',
-      result: 'W'
-    },
-    {
-      teamName: 'URUGUAY',
-      result: 'L'
-    }
-  ]
-},
-{
-  data: [
-    {
-      teamName: 'AUSTRALIA',
-      result: 'W'
-    },
-    {
-      teamName: 'PORTUGAL',
-      result: 'L'
-    }
-  ]
-},
-{
-  data: [
-    {
-      teamName: 'FRANCE',
-      result: 'W'
-    },
-    {
-      teamName: 'AUSTRALIA',
-      result: 'L'
-    }
-  ]
-}
+  {
+    data: [
+      {
+        teamName: 'FRANCE',
+        result: 'W'
+      },
+      {
+        teamName: 'NEW_ZEALAND',
+        result: 'L'
+      }
+    ]
+  },
+  {
+    data: [
+      {
+        teamName: 'ITALY',
+        result: 'L'
+      },
+      {
+        teamName: 'URUGUAY',
+        result: 'W'
+      }
+    ]
+  },
+  {
+    data: [
+      {
+        teamName: 'SOUTH_AFRICA',
+        result: 'L'
+      },
+      {
+        teamName: 'AUSTRALIA',
+        result: 'W'
+      }
+    ]
+  },
+  {
+    data: [
+      {
+        teamName: 'PORTUGAL',
+        result: 'W'
+      },
+      {
+        teamName: 'ARGENTINA',
+        result: 'L'
+      }
+    ]
+  },
+  {
+    data: [
+      {
+        teamName: 'FRANCE',
+        result: 'W'
+      },
+      {
+        teamName: 'URUGUAY',
+        result: 'L'
+      }
+    ]
+  },
+  {
+    data: [
+      {
+        teamName: 'AUSTRALIA',
+        result: 'W'
+      },
+      {
+        teamName: 'PORTUGAL',
+        result: 'L'
+      }
+    ]
+  },
+  {
+    data: [
+      {
+        teamName: 'FRANCE',
+        result: 'W'
+      },
+      {
+        teamName: 'AUSTRALIA',
+        result: 'L'
+      }
+    ]
+  }
 
 ]
 
--- app/knockouts/page.tsx
+++ app/knockouts/page.tsx
@@ -1,5 +1,3 @@
-import Knockout from '@/components/knockouts/knockout'
-import { TeamsData } from './knockoutslayout'
 import MatchResults from '@/components/knockouts/matchResults'
 
 export default async function KnochoutPage() {
