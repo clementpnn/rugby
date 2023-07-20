e42430b5f1318e317fdba90b8a7928fccad58642
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2023-07-20 02:55:52 +0200

    🚧Begin profile page

---
+++ app/profilePage/page.tsx
@@ -0,0 +1,25 @@
+import Navbar from '@/components/navbar/navbar'
+import Filter from '@/components/filter/filter'
+import Empty from '@/components/containers/empty'
+// import getMatch from '@/actions/getMatch'
+
+const page = async () => {
+  // const matchs = await getMatch()
+  // eslint-disable-next-line no-console
+  // console.log( matchs )
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar />
+      <div className="grid grid-cols-3 h-[calc(100%-101px)]">
+        <div className="col-span-2 w-full h-full flex-1 border-r-[1px] lg:border-r-1">
+          <Empty />
+        </div>
+        <div className="w-full h-full col-span-1">
+          {/*<Filter height={0} width={0}/> */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
