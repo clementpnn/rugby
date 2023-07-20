0f8ab28387ded271ad9170f6cd0856cec74af544
Victor Huang <huang.victor.mail@gmail.com>
2023-07-20 23:09:51 +0200

    MatchList admin

--- app/admin/match/matchList.tsx
+++ app/admin/match/matchList.tsx
@@ -1,53 +1,11 @@
 'use client'
 
-import { Match } from '@/components/modals/modalJoinWaitList'
-
-// interface MatchListProperties {
-//   matchs: ( Match & { matchTeams: MatchTeam[] } )[]
-// }
-
-interface MatchsByDate {
-  [date: string]: Match[]
-}
-
-interface MatchListProperties {
-    matchs: MatchsByDate
-}
-
-const MatchList: React.FC<MatchListProperties> = ( { matchs } ) => {
-
-  return (
-    <>
-      {matchs}
-      {/* {matchs.map( ( match ) => {
-        return (
-          <Link href={`/admin/match/${match.id}`} key={match.id}> */}
-      {/* <h1 className='mt-6'>{match.phase}</h1>
-            <p>{match.date}</p>
-            <p>{match.time}</p>
-            {match.matchTeams.map( ( team ) => {
-              return (
-                <div key={team.id}>
-                  <p>{team.team}</p>
-                  <p>{team.result ?? '?'}</p>
-                </div>
-              )
-            } ) } */}
-      {/* <RequestMatch data={match} state='disabled' stateClass='disabled' time='25:30'></RequestMatch>
-          </Link>
-        )
-      } ) } */}
-    </>
-  )
-}
-
-export default MatchList
-
-'use client'
-
 import Container from '@/components/containers/container'
+import Empty from '@/components/containers/empty'
 import { Match } from '@/components/modals/modalJoinWaitList'
 import RequestMatch from '@/components/requestMatch/requestMatch'
+import { useFilterStore } from '@/hooks/useFilter'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 // interface MatchListProperties {
 //   matchs: ( Match & { matchTeams: MatchTeam[] } )[]
@@ -110,7 +68,7 @@ const MatchList: React.FC<MatchListProperties> = ( { matchs } ) => {
               <div className='border-y-[1px] border-neutral3 divide-y divide-neutral3'>
                 {matches.map( ( match: Match ) => (
                   <div key={match.id}>
-                    <RequestMatch data={match} />
+                    <RequestMatch data={match} state='disabled' stateClass='disabled' time='25:30'></RequestMatch>
                   </div>
                 ) )}
               </div>
