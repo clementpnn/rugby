c614b2703ec60d56d55a7561b4237e22062530c8
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2023-07-20 02:58:28 +0200

    🐛update lint

--- app/profilePage/page.tsx
+++ app/profilePage/page.tsx
@@ -1,5 +1,4 @@
 import Navbar from '@/components/navbar/navbar'
-import Filter from '@/components/filter/filter'
 import Empty from '@/components/containers/empty'
 // import getMatch from '@/actions/getMatch'
 
